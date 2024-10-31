b0008b828667f963bbff1013385496978c4c2203
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-10-31 14:01:24 +0530

    Add Framer Motion to Contact and Nav Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,45 @@
 import { CONTACT } from "../constants"
+import { motion } from "framer-motion"
 
 const Contact = () => {
     return (
         <div className="border-b border-neutral-900 pb-4">
-            <h2 className="my-20 text-center text-4xl">Get in Touch</h2>
+            <motion.h2 
+            whileInView={{opacity:1,y:0}}
+            initial={{opacity:0,y:-100}}
+            transition={{duration:0.5}}
+            className="my-20 text-center text-4xl">Get in Touch</motion.h2>
             <div className="text-center tracking-tighter">
-                <p className="my-4">
+                <motion.p 
+                whileInView={{opacity:1,x:0}}
+                initial={{opacity:0,x:-100}}
+                transition={{duration:1}}
+                className="my-4">
                     {CONTACT.address}
-                </p>
-                <p className="my-4">
+                </motion.p>
+                <motion.p 
+                whileInView={{opacity:1,x:0}}
+                initial={{opacity:0,x:100}}
+                transition={{duration:1}}
+                className="my-4">
                     {CONTACT.phoneNo}
-                </p>
-                <a href="mailto:gamalathrashini@gmail.com" className="border-b mb-5">
+                </motion.p>
+                <motion.a 
+                whileTap={{ scale: 0.8 }}
+                href="mailto:gamalathrashini@gmail.com" className="border-b mb-5">
                     {CONTACT.email}
-                </a><br></br>
+                </motion.a><br></br>
 
-                <a
+                <motion.a
+                whileHover={{ scale: 1.2 }}
+                 whileTap={{ scale: 0.8 }}
                     href="assets/doc/Rashini Sathsarani Gamalath.pdf"
                     download
                     target="_blank"
                     className="mt-5 inline-block px-6 py-2 text-white bg-purple-600 rounded-lg font-semibold hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
                 >
                     Download CV
-                </a>
+                </motion.a>
 
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,26 @@
 import logo from '../assets/RsLogo.png';
 import { FaLinkedin, FaGithub} from 'react-icons/fa';
 import { FaMedium } from 'react-icons/fa6';
+import { motion } from 'framer-motion';
 const Navbar = () => {
   return <nav className=" mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
       <img className='mx-2 w-10' src={logo} alt="logo" />
     </div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-      <a href="https://www.linkedin.com/in/rashini-gamalath-3b27b41b2/" target="_blank" rel="noopener noreferrer">
+      <motion.a 
+       whileHover={{ scale: 2 }}
+       href="https://www.linkedin.com/in/rashini-gamalath-3b27b41b2/" target="_blank" rel="noopener noreferrer">
         <FaLinkedin />
-      </a>
-      <a href="https://github.com/Rashini1998" target="_blank" rel="noopener noreferrer">
+      </motion.a>
+      <motion.a 
+       whileHover={{ scale: 2 }} href="https://github.com/Rashini1998" target="_blank" rel="noopener noreferrer">
         <FaGithub />
-      </a>
-      <a href="https://gamalathrashini.medium.com/" target="_blank" rel="noopener noreferrer">
+      </motion.a>
+      <motion.a 
+       whileHover={{ scale: 2 }} href="https://gamalathrashini.medium.com/" target="_blank" rel="noopener noreferrer">
         <FaMedium />
-      </a>
+      </motion.a>
     </div>
   </nav>
 }
