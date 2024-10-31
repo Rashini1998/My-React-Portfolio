a8c0cc55a2a7b6ac2e775c88ec29c9328aae657b
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-10-31 08:46:34 +0530

    Did some changes to image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center">
-                        <img src={profilePic} alt="Rashini Gamalath"/>
+                        <img className="rounded-2xl" src={profilePic} alt="Rashini Gamalath"/>
                     </div>
                 </div>
             </div>
